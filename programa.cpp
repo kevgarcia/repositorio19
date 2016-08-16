aea9dda2ece328a5ae15ab488de2bc23ce960424
kevgarcia <kevnoegarcia@gmail.com>
2016-08-15 18:25:20 -0600

    Se creo un programa basico en c++.  Su binario .exe ha sido compilado y puede ejecutarse en Windows

---
+++ programa.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	float a, b, suma;
+	cout<<"Ingrese dos numeros : "<<endl;
+	cin>>a >> b;
+	
+	suma = a + b;
+	
+	cout<<"La suma de los dos numeros es: "<<suma;
+}
